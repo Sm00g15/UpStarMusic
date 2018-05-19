235d2a670c4942b8b552cf94b14f7b6ad2c107d7
Sean M <sean@arrowburn.co.nz>
2018-05-19 13:20:01 +0200

    setup album and artist schemas

--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,19 @@
 // Todo: Create Artist Model
+const mongoose = require('mongoose');
+const AlbumSchema = require('./album')
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+	name: String,
+	age: Number,
+	yearsActive: Number,
+	image: String,
+	genre: String,
+	website: String,
+	netWorth: Number,
+	labelName: String, 
+	retired: String,
+	Albums: [AlbumSchema]
+});
+const Artist = mongoose.model('Artist', ArtistSchema)
+module.exports = Artist;
